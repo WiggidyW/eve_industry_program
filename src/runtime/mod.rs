a6d5ab3bd3cb2ed3cd4cedb4490aeee2d52ab56d
Ronald Riggles <rigglesr@dot.state.al.us>
2024-08-01 16:55:59 -0500

    asdf

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,3 +1,8 @@
+use crate::{
+    api_data_rename as api_data,
+    config::{self, Item},
+    industry_db,
+};
 use std::{collections::HashMap, rc::Rc};
 
 mod delivery_route;
@@ -19,4 +24,31 @@ use market_orders::*;
 pub struct RuntimeData<'cfg, 'db, 'api> {
     pub locations: Vec<Rc<Location<'cfg, 'db, 'api>>>,
     pub type_volumes: &'db HashMap<u32, f64>,
+}
+
+impl<'cfg, 'db, 'api> RuntimeData<'cfg, 'db, 'api> {
+    pub fn new(
+        cfg_locations: &'cfg [config::Location],
+        db_lines: &'db HashMap<u32, industry_db::Line>,
+        type_volumes: &'db HashMap<u32, f64>,
+        adjusted_prices: &'api HashMap<u32, f64>,
+        cost_indices: &'api HashMap<u32, config::ManufacturingValue>,
+        market_orders: &'api HashMap<
+            u64,
+            HashMap<u32, api_data::TypeMarketOrders>,
+        >,
+        assets: &'api HashMap<u64, HashMap<Item, i64>>,
+    ) -> Self {
+        Self {
+            locations: new_locations(
+                cfg_locations,
+                db_lines,
+                adjusted_prices,
+                cost_indices,
+                market_orders,
+                assets,
+            ),
+            type_volumes,
+        }
+    }
 }
